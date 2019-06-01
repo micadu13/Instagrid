b05867a491b3a897504cf9f247d4c5765d58e91d
michael aidan <mica.aidan@gmail.com>
2019-06-01 10:42:14 +0200

    portrait mode ok passing to landscape

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Michael Aidan on 25/05/2019.
+//  Copyright © 2019 Michael Aidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
