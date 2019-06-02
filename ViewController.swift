f1f527b50e07a03e5e69027a55e73f0b5b7a1cc0
michael aidan <mica.aidan@gmail.com>
2019-06-02 19:43:30 +0200

    created outlets and actions

--- ViewController.swift
+++ ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var FirstPicture: UIButton!
+    @IBOutlet weak var SecondPicture: UIButton!
+    @IBOutlet weak var ThirdPicture: UIButton!
+    @IBOutlet weak var FourthPicture: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    //Actions
 
-
+    @IBAction func FirstButton(_ sender: Any) {
+    }
+    
+    @IBAction func SecondButton(_ sender: Any) {
+    }
+    
+    @IBAction func ThirdButton(_ sender: Any) {
+    }
+    
 }
 
