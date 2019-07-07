0eefe16ad05b4c2fb833ed42f976305d9429ddf9
michael aidan <mica.aidan@gmail.com>
2019-07-07 02:55:43 +0200

    implementing swipe

--- ViewController.swift
+++ ViewController.swift
@@ -10,94 +10,145 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
  
-    //Outlets
-    @IBOutlet weak var firstPicture: UIButton!
-    @IBOutlet weak var secondPicture: UIButton!
-    @IBOutlet weak var thirdPicture: UIButton!
-    @IBOutlet weak var fourthPicture: UIButton!
-    @IBOutlet weak var toswipe: UILabel!
     
+    
+    //Outlets
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    @IBOutlet weak var fourthButton: UIButton!
+    @IBOutlet weak var toswipe: UILabel!
+    @IBOutlet weak var stackgrille: UIView!
+    var tagselected:Int?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        imagepicker.delegate = self
         // Do any additional setup after loading the view.
-        
     }
     
+    func factorisation(sender:UIButton){// On reçoit le paramètre sender qui a été envoyé depuis la fonction firstpictchosen
+        tagselected = sender.tag// On stocke l'identifiant du bouton cliqué
+        addImage()
+    }
     
-    
-    
-     let imagepicker = UIImagePickerController()
+ 
     
        //Action to choose pict
     
-    @IBAction func firstpictchosen(_sender: Any) {
-        imagepicker.sourceType = .photoLibrary
-        imagepicker.allowsEditing = true
-        present(imagepicker, animated: true,completion: nil)
-
+    @IBAction func firstpictchosen(_ sender: Any) {//On reçoit le boutton clické en tant que paramètre : sender
+        let sent = sender as! UIButton// On va convertir ce paramètre vers un bouton
+        factorisation(sender: sent)// On va passer ce boutton à la méthode factorisation qui va l'utiliser pour récupérer le tag
+        
+    }
+    
+    @IBAction func secondpictchosen(_ sender: Any) {
+        let sent = sender as! UIButton
+        factorisation(sender: sent)
+    }
+    
+    @IBAction func thirdpictchosen(_ sender: Any) {
+        let sent = sender as! UIButton
+        factorisation(sender: sent)
+    }
+    
+    @IBAction func thourthpictchosen(_ sender: Any) {
+        let sent = sender as! UIButton
+        factorisation(sender: sent)
     }
     
     
-@objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func addImage() {
+        let image = UIImagePickerController()
+        image.delegate = self
+        image.sourceType = .photoLibrary
+        image.allowsEditing = true
+        self.present(image, animated: true)
+    }
+    
+   
+    func exportgrille(){
+        //implementer la detection du swipe
+        // 1 Instancier le swipe ou gesture recognizer
+        // 2 Configurer le gesture recognizer
+        // 3 Associer le swipe à la grille
+        // 4 Exporter
+    }
+    
+    func share(){
+        //5  Proposer de partager
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true)
         
-        guard let image = info[.editedImage] as? UIImage else {
-            print("No image found")
-            return
+        if let image = info[.editedImage] as? UIImage{
+            //firstButton.setImage(image, for: .normal)
+            
+            switch tagselected {
+            case 1:
+                firstButton.setImage(image, for: .normal)
+            case 2:
+                secondButton.setImage(image, for: .normal)
+            case 3:
+                thirdButton.setImage(image, for: .normal)
+            case 4:
+                fourthButton.setImage(image, for: .normal)
+            break
+            default:
+                print("OK")
+            }
+            
+                                                    }
+        
+        
+        
         }
     
-        print(image)
-    }
     
- //Action for bottom app
-    @IBAction func firstButton() {
-      firstButtonctivated()
-    }
-    
-    
-    @IBAction func secondButton() {
-         secondButtonactivated()
-    }
-    @IBAction func ThirdButton() {
-        thirdButtonactivated()
-    }
-    
-    // Functions
-    
-    //first button
-    private func firstButtonctivated() {
-        firstPicture.isHidden = false
-        secondPicture.isHidden = true
-        thirdPicture.isHidden = false
-        fourthPicture.isHidden = false
-        
-    }
-    //Second button
-    private func secondButtonactivated()
-    {
-        firstPicture.isHidden = false
-        secondPicture.isHidden = false
-        thirdPicture.isHidden = false
-        fourthPicture.isHidden = true
-        
-    }
-    
-    
-    //Third button
-    
-    private func thirdButtonactivated()
-    {
-        firstPicture.isHidden = false
-        secondPicture.isHidden = false
-        thirdPicture.isHidden = false
-        fourthPicture.isHidden = false
-        
-    }
     
 
+    //Action for bottom app
+    
+    @IBAction func firstPosition() {
+        firstButtonactivated()
+    }
+    
+    
+    @IBAction func secondPosition() {
+        secondButtonactivated()
+    }
+    
+    
+    @IBAction func thirdPosition() {
+        thirdButtonactivated()
+    }
+
+    func firstButtonactivated () {
+        firstButton.isHidden = false
+        secondButton.isHidden = true
+        thirdButton.isHidden = false
+        fourthButton.isHidden = false
+    
+    }
+    
+    func secondButtonactivated()
+    {
+        firstButton.isHidden = false
+        secondButton.isHidden = false
+        thirdButton.isHidden = false
+        fourthButton.isHidden = true
+    }
+    
+    func thirdButtonactivated(){
+        firstButton.isHidden = false
+        secondButton.isHidden = false
+        thirdButton.isHidden = false
+        fourthButton.isHidden = false
+    }
+
+    @IBAction func swipeimage(_ sender: UISwipeGestureRecognizer) {
+    }
 }
 
 
