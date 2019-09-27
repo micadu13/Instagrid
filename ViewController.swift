d56ebf58b64aaa32bdbbc2edfb9e74b4d3991997
michael aidan <mica.aidan@gmail.com>
2019-09-27 08:06:14 +0200

    rewrote some functions for camel case hommage

--- ViewController.swift
+++ ViewController.swift
@@ -43,12 +43,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         //putting in a constance the position value of the grid
         let yposition_min = stackgrille.frame.minY
         //Implementation of the translation
-        var translationTransform: CGAffineTransform
-        translationTransform = CGAffineTransform(translationX:0, y: -800)
-        UIView.animate(withDuration: 3, animations: {
-            self.stackgrille.transform = translationTransform
-        }, completion: nil)
-        
+        animateGridVertically(-800)
         //Creates a bitmap-based graphics context with the specified options.
         UIGraphicsBeginImageContextWithOptions(stackgrille.bounds.size, true, 0)
         stackgrille.drawHierarchy(in: stackgrille.bounds, afterScreenUpdates: true)
@@ -56,53 +51,59 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         //Returns an image based on the contents of the current bitmap-based graphics context.
         let image = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
+        
         let items = [image]
         
         //Starting UIAcitivyViewController
         let ac = UIActivityViewController(activityItems: items as [Any], applicationActivities: nil)
         ac.completionWithItemsHandler = {(activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
+            self.animateGridVertically(yposition_min)
+     }
+        present(ac, animated: true)
+   
+    }
+
+    
+    func animateGridVertically(_ position: CGFloat) {
         var translationTransform: CGAffineTransform
         
         //stackgrille is coming back after to its inital position
-        translationTransform = CGAffineTransform(translationX:0, y: yposition_min)
+        translationTransform = CGAffineTransform(translationX:0, y: position)
         UIView.animate(withDuration: 3, animations: {
-        self.stackgrille.transform = translationTransform
+            self.stackgrille.transform = translationTransform
         }, completion: nil)
-  
-        }
-        self.present(ac, animated: true)
-   
+    
     }
     
 //---------------------------------------------Replacing images to a button-----------------------------------------------------------------
 
 //Factorisating the code
-  func factorisation(sender:UIButton){// We receive the parameter "sender" that has been sent from function firstpictchosen
+  func prepareForImagePicker(sender:UIButton){// We receive the parameter "sender" that has been sent from function firstPictChosen
         tagselected = sender.tag// We storage the id of the clicked button
         addImage()
     }
 
 //Action to choose pict
     
-    @IBAction func firstpictchosen(_ sender: Any) {// We receive the clicked button as parameter : sender
+    @IBAction func firstPictChosen(_ sender: Any) {// We receive the clicked button as parameter : sender
         let sent = sender as! UIButton// We are are going to convert this parameter through a button
-        factorisation(sender: sent)// We are going to pass the button to the method "factorisation" which is goig to pick up the tag
+        prepareForImagePicker(sender: sent)// We are going to pass the button to the method "factorisation" which is goig to pick up the tag
     }
 // We make the same thing for the 3 other pictures
     
-    @IBAction func secondpictchosen(_ sender: Any) {
+    @IBAction func secondPictChosen(_ sender: Any) {
         let sent = sender as! UIButton
-        factorisation(sender: sent)
+        prepareForImagePicker(sender: sent)
     }
     
-    @IBAction func thirdpictchosen(_ sender: Any) {
+    @IBAction func thirdPictChosen(_ sender: Any) {
         let sent = sender as! UIButton
-        factorisation(sender: sent)
+        prepareForImagePicker(sender: sent)
     }
     
-    @IBAction func thourthpictchosen(_ sender: Any) {
+    @IBAction func thourthPictChosen(_ sender: Any) {
         let sent = sender as! UIButton
-        factorisation(sender: sent)
+        prepareForImagePicker(sender: sent)
     }
     
     //  Function to replace image instead of button
@@ -114,19 +115,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         self.present(image, animated: true)
     }
     
-    // MARK: - Swift updated
-    // Helper function inserted by Swift 4.2 migrator.
-    fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
-        return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
-    }
-    
-    // Helper function inserted by Swift 4.2 migrator.
-    fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-        return input.rawValue
-    }
-    
-    
-    
+
     // Able to take the pictures and to replace it instead of button
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         picker.dismiss(animated: true)
@@ -155,21 +144,21 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     // Actions to change the view of the app with the buttons
     
     @IBAction func firstPosition() {
-        firstButtonactivated()
+        firstButtonActivated()
     }
     
     
     @IBAction func secondPosition() {
-        secondButtonactivated()
+        secondButtonActivated()
     }
     
     
     @IBAction func thirdPosition() {
-        thirdButtonactivated()
+        thirdButtonActivated()
     }
 
 //---------------------------------------------Parametring disposition of the app thanks to the property "is hidden"-------------------------------
-    func firstButtonactivated () {
+    func firstButtonActivated () {
         firstButton.isHidden = false
         secondButton.isHidden = true
         thirdButton.isHidden = false
@@ -177,7 +166,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         
     }
     
-    func secondButtonactivated()
+    func secondButtonActivated()
     {
         firstButton.isHidden = false
         secondButton.isHidden = false
@@ -185,7 +174,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         fourthButton.isHidden = true
     }
     
-    func thirdButtonactivated(){
+    func thirdButtonActivated(){
         firstButton.isHidden = false
         secondButton.isHidden = false
         thirdButton.isHidden = false
